af9f8debe44135d29cb201ad906b3361a20a8fdf
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2023-04-12 10:21:34 +0200

    add post route and useSWRMutation

--- components/JokeForm.js/index.js
+++ components/JokeForm.js/index.js
@@ -1,6 +1,36 @@
+import useSWRMutation from "swr/mutation";
+
+async function sendRequest(url, { arg }) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(arg),
+  });
+
+  const { status } = await response.json();
+  console.log(status);
+}
+
 export default function JokeForm() {
+  const { trigger } = useSWRMutation("/api/jokes", sendRequest);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // const joke = event.target.joke.value;
+
+    const formData = new FormData(event.target);
+    const jokeData = Object.fromEntries(formData);
+
+    trigger(jokeData);
+    // 1. trigger sendRequest, pass jokeData as { arg } to it
+    // 2. update all useSWR hooks that subscribed to '/api/jokes/'
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="joke-input">Enter a new joke</label>
       <input type="text" id="joke-input" name="joke" />
       <button type="submit">Submit</button>
--- pages/api/jokes/index.js
+++ pages/api/jokes/index.js
@@ -8,4 +8,15 @@ export default async function handler(request, response) {
     const jokes = await Joke.find();
     return response.status(200).json(jokes);
   }
+
+  if (request.method === "POST") {
+    const jokeData = request.body; // expects to be { joke: "asdlfjsadf" }
+    await Joke.create(jokeData);
+    response.status(200).json({ status: "Joke created!" });
+  }
 }
+
+// Joke.create();
+// Joke.find();
+// Joke.findByIdAndUpdate();
+// Joke.findByIdAndDelete();
